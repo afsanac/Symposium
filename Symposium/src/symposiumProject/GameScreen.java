a3d243f07541decd78338f47f254d2ba6ddad785
Student7 <Student7@10.8.33.144>
2017-05-17 12:28:33 -0400

    ccc`2w

--- Symposium/src/symposiumProject/GameScreen.java
+++ Symposium/src/symposiumProject/GameScreen.java
@@ -29,28 +29,31 @@ public class GameScreen extends ClickableScreen implements Runnable {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		title = new TextLabel(300, 100, 200, 50, "Faery Adventure");
+		title = new TextLabel(300, 100, 200, 50, "Fairly Oddventure");
 		author = new TextLabel(300, 150, 250, 50, "By Afsana Chadni");
-		intro = new TextLabel(50, 100, 700, 300, "Welcome To FaeryLand" + "My name is Pixie and I'll be your guide");
-		
-		gameStarter = new Button(300, 200, 200, 50, "Start Game", Color.CYAN, new Action(){
+		intro = new TextLabel(50, 100, 700, 300, "Welcome To FaeryLand " + "My name is Pixie and I'll be your guide");
 
-			public void act(){
+		gameStarter = new Button(300, 200, 200, 50, "Start Game", Color.CYAN, new Action() {
+
+			public void act() {
 				viewObjects.remove(title);
 				viewObjects.remove(author);
 				viewObjects.remove(gameStarter);
 				viewObjects.add(intro);
-				exitIntro = new Button(400, 400, 200, 50, "Exit", Color.DARK_GRAY, new Action(){
-					
-					public void act(){
+				exitIntro = new Button(400, 400, 200, 50, "Exit", Color.DARK_GRAY, new Action() {
+
+					public void act() {
 						remove(intro);
 						remove(exitIntro);
 					}
 				});
-				
+
 			}
 		});
-		
+		viewObjects.add(title);
+		viewObjects.add(author);
+		viewObjects.add(intro);
+
 	}
 
 }
